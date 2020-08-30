984bf3d55c6a50a098cee4a46b61749c8099b1dd
yangs1202 <me@yangs.kr>
2020-08-30 17:32:56 +0900

    지역 리스트는 상수로 전환

--- src/main/java/kr/quantumsoft/koreasurvey/utils/ProjectConstants.java
+++ src/main/java/kr/quantumsoft/koreasurvey/utils/ProjectConstants.java
@@ -15,7 +15,10 @@
  */
 package kr.quantumsoft.koreasurvey.utils;
 
-/**  
+import java.util.Arrays;
+import java.util.List;
+
+/**
  * @Class Name : ProjectConstants.java
  * @Project Name : koreasurvey
  * @Description : 
@@ -45,4 +48,7 @@ public class ProjectConstants {
 	public static final Integer TRADING_CASH = 0;
 	public static final Integer TRADING_CARD = 1;
 	public static final Integer TRADING_SUBMIT = 2;
+
+	public static final List<String> REGION_STRINGS = Arrays.asList("서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도");
+
 }
