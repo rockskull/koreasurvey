3ba6ded1afce5874e872ca8a1f9a3cddb449a36a
yangs1202 <me@yangs.kr>
2020-09-22 12:23:38 +0900

    WIP : Admin

--- src/main/java/kr/quantumsoft/koreasurvey/controller/AdminController.java
+++ src/main/java/kr/quantumsoft/koreasurvey/controller/AdminController.java
@@ -4,6 +4,7 @@ import kr.quantumsoft.koreasurvey.model.Tradings;
 import kr.quantumsoft.koreasurvey.model.Users;
 import kr.quantumsoft.koreasurvey.service.TradingsService;
 import kr.quantumsoft.koreasurvey.service.UsersService;
+import kr.quantumsoft.koreasurvey.utils.ProjectConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,12 +27,22 @@ public class AdminController {
     @Autowired
     private TradingsService tradingsService;
 
-    private static final List<String> REGION_STRINGS = Arrays.asList("서울특별시", "부산광역시", "대구광역시", "인천광역시", "광주광역시", "대전광역시", "울산광역시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주특별자치도");
 
 
     @RequestMapping("")
     public ModelAndView index() {
         return new ModelAndView("admin/index");
+    }
+
+    @RequestMapping("notice/list")
+    public ModelAndView notices() {
+        Map<String, Object> items = new HashMap<String, Object>();
+//        items.put("data", userService.search(email, start, end));
+//        items.put("start", start);
+//        items.put("end", end);
+//        items.put("email", email);
+
+        return new ModelAndView("admin/notice/list", items);
     }
 
     @RequestMapping("user/list")
@@ -61,7 +72,7 @@ public class AdminController {
         }
         Map<String, Object> items = new HashMap<String, Object>();
         items.put("data", user);
-        items.put("regions", REGION_STRINGS);
+        items.put("regions", ProjectConstants.REGION_STRINGS);
         return new ModelAndView("admin/users/detail", items);
     }
 
